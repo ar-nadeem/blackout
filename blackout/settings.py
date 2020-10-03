f93acf65d0f648ead8aa9866c9fbbfa097392734
AbdulRahman Nadeem <ar-nadeem@outlook.com>
2020-10-03 21:32:55 +0500

    asd

--- blackout/settings.py
+++ blackout/settings.py
@@ -130,4 +130,4 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/staticfiles//'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
