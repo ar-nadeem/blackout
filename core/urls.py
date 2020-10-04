0cedc5fca88724d1d560d48162ce41c1c19efcf6
AbdulRahman Nadeem <ar-nadeem@outlook.com>
2020-10-04 15:03:51 +0500

    Done

--- core/urls.py
+++ core/urls.py
@@ -6,16 +6,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-router = routers.DefaultRouter()
-router.register(r'heroes', views.HeroViewSet)
-router.register(r'solar', views.SolarViewSet)
 
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('rest/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
 
 
 
--- core/views.py
+++ core/views.py
@@ -3,20 +3,70 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import HeroSerializer, SolarSerializer
 from .models import Hero, Solar
+from django.shortcuts import render
 
 from django.http import HttpResponse
 
 
 def main(request):
-    line = "Hello World"
-    Solar.objects.all().delete()
-    return HttpResponse(line)
+    import http.client
+    import json
+
+    conn = http.client.HTTPSConnection('api.pipedream.com')
+    conn.request("GET", '/v1/sources/dc_lVuxEY/event_summaries?limit=1', '', {
+        'Authorization': 'Bearer b6283d5b52f9bc13ac965d5c8282ad60',
+    })
+
+    res = conn.getresponse()
+    data = res.read()
+    body = data.decode("utf-8")
+    body = body[body.find('"{\\"mode') + 1:body.find('"metadata') - 2]
+    body = body.replace("\\", "")
+    body = body.replace(" ", "")
 
 
-class HeroViewSet(viewsets.ModelViewSet):
-    queryset = Hero.objects.all().order_by('name')
-    serializer_class = HeroSerializer
+    def get_me(x):
+        x = body[body.find('"'+x+'":') + 3+len(x):]
+        x = x[:x.find(",")]
+        return x
 
-class SolarViewSet(viewsets.ModelViewSet):
-    queryset = Solar.objects.all().order_by('gridvoltage')
-    serializer_class = SolarSerializer
+
+    mode = get_me("mode")
+    if mode == "Sola":
+        mode = "Solar / Battery"
+    else:
+        mode = "Grid"
+
+
+    stuff_for_frontend = {
+        'mode': mode,
+
+        'gridVoltage': get_me("gridVoltage"),
+        'gridFreq': get_me("gridFreq"),
+        'outputVoltage': get_me("outputVoltage"),
+        'outputFreq': get_me("outputFreq"),
+
+        'outputPowerApparent': get_me("outputPowerApparent"),
+        'outputPowerActive': get_me("outputPowerActive"),
+        'loadPercent': get_me("loadPercent"),
+
+        'busVoltage': get_me("busVoltage"),
+        'batteryVoltage': get_me("batteryVoltage"),
+        'batteryCurrent': get_me("batteryCurrent"),
+        'batteryCapacity': get_me("batteryCapacity"),
+        'batteryVoltageSCC': get_me("batteryVoltageSCC"),
+        'batteryDischargeCurrent': get_me("batteryDischargeCurrent"),
+
+        'temperature': get_me("temperature"),
+        'solarCurrent': get_me("solarCurrent"),
+        'solarVoltage': get_me("solarVoltage"),
+
+
+        'status': get_me("status"),
+        'warning': get_me("warning"),
+
+
+
+    }
+
+    return render(request, 'index.html', stuff_for_frontend)
